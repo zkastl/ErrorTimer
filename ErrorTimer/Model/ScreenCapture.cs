cf8619a3bc9c4d16c595791ca1a7cecddb9e74a3
Zachary Kastl <zkastl8@gmail.com>
2017-07-05 12:37:12 -0500

    Intial Commit

---
+++ ErrorTimer/Model/ScreenCapture.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ErrorTimer.Model
+{
+    public static class ScreenCapture
+    {
+        public static Bitmap CaptureScreens()
+        {
+            var allScreens = Screen.AllScreens.OrderBy(s => s.Bounds.Left).ToArray();
+            var totalWidth = allScreens.Sum(s => s.Bounds.Width);
+            var maxHeight = allScreens.Max(s => s.Bounds.Top + s.Bounds.Height);
+
+            Bitmap bmp = new Bitmap(totalWidth, maxHeight);
+            int offset = 0;
+
+            using (var g = Graphics.FromImage(bmp))
+            {
+                foreach (var screen in allScreens)
+                {
+                    g.CopyFromScreen(screen.Bounds.Left, screen.Bounds.Top, offset,
+                        screen.Bounds.Top, screen.Bounds.Size);
+                    offset += screen.Bounds.Width;
+                }
+            }
+            return bmp;
+        }
+    }
+}
---
+++ ErrorTimer/View/ErrorTimerControl.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+using ErrorTimer.Model;
+using SlimBindableCommand;
+
+
+namespace ErrorTimer.View
+{
+    /// <summary>
+    /// Interaction logic for ErrorTimerControl.xaml
+    /// </summary>
+    public partial class ErrorTimerControl : UserControl
+    {
+        private ErrorTimerControlViewModel etcvm;
+        public ErrorTimerControl()
+        {
+            etcvm = new ErrorTimerControlViewModel();
+            DataContext = etcvm;
+            InitializeComponent();
+        }
+    }
+
+    public class ErrorTimerControlViewModel : ProtoBind
+    {
+        public bool startIsEnabled
+        {
+            get { return ObservableGet<bool>(); }
+            set { ObservableSet(value); }
+        }
+        public bool reportIsEnabled
+        {
+            get { return ObservableGet<bool>(); }
+            set { ObservableSet(value); }
+        }
+        public bool stopIsEnabled
+        {
+            get { return ObservableGet<bool>(); }
+            set { ObservableSet(value); }
+        }
+        public ICommand startButtonClickCommand { get; private set; }
+        public ICommand reportButtonClickCommand { get; private set; }
+        public ICommand stopButtonClickCommand { get; private set; }
+
+        private Stopwatch sw;
+        private Dictionary<string, Bitmap> savedScreenshots;
+
+        public ErrorTimerControlViewModel()
+        {
+            startIsEnabled = true;
+            reportIsEnabled = false;
+            stopIsEnabled = false;
+            startButtonClickCommand = new ProtoSyncCommand(startButtonClick);
+            reportButtonClickCommand = new ProtoSyncCommand(reportButtonClick);
+            stopButtonClickCommand = new ProtoSyncCommand(stopButtonClick);
+            sw = new Stopwatch();
+            savedScreenshots = new Dictionary<string, Bitmap>();
+        }
+
+        private void startButtonClick()
+        {
+            startIsEnabled = false;
+            reportIsEnabled = true;
+            stopIsEnabled = true;
+
+            sw.Restart();
+        }
+
+        private void reportButtonClick()
+        {
+            savedScreenshots.Add(sw.Elapsed.ToString(), ScreenCapture.CaptureScreens());
+        }
+
+        private void stopButtonClick()
+        {
+            sw.Stop();
+            startIsEnabled = true;
+            reportIsEnabled = false;
+            stopIsEnabled = false;
+            SaveScreenshotsToFileSystem();
+        }
+
+        private void SaveScreenshotsToFileSystem()
+        {
+            string screenshotPath = AppDomain.CurrentDomain.BaseDirectory + "SHTS_" + DateTime.Now.Ticks.ToString();
+            Directory.CreateDirectory(screenshotPath);
+            foreach(KeyValuePair<string, Bitmap> kvp in savedScreenshots)
+            {
+                kvp.Value.Save(screenshotPath + "\\" + kvp.Key.Replace(':', '-') + ".bmp", System.Drawing.Imaging.ImageFormat.Png);                
+            }
+
+            Process.Start(screenshotPath);
+        }
+    }
+}
